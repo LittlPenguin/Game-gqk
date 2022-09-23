3914c81b196f85122608f970e2361e6425d0ae30
YnaQuI <86390808+LittlPenguin@users.noreply.github.com>
2022-09-23 12:29:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-// 工具封装
-// 根据id来获取元素
 function $(id) {
   return document.getElementById(id);
 }
 
-// 创建div, className是其类名
 function creatediv(className) {
   var div = document.createElement("div");
   div.className = className;
@@ -15,7 +12,6 @@ var state = 0;
 var speed = 6;
 var flag = false;
 
-//点击开始游戏按钮 开始游戏
 function start() {
   if (!flag) {
     init();
@@ -24,43 +20,34 @@ function start() {
   }
 }
 
-/*
- *    初始化 init
- */
 function init() {
   flag = true;
   for (var i = 0; i < 4; i++) {
     createrow();
   }
 
-  // 添加onclick事件
   $("main").onclick = function (ev) {
     ev = ev || event;
     judge(ev);
   };
 
-  // 定时器 每30毫秒调用一次move()
   clock = window.setInterval("move()", 30);
 }
 
-// 判断是否点击黑块、白块
 function judge(ev) {
   if (
     ev.target.className.indexOf("black") == -1 &&
     ev.target.className.indexOf("cell") !== -1
   ) {
-    ev.target.parentNode.pass1 = 1; //定义属性pass，表示此行row的白块已经被点击
+    ev.target.parentNode.pass1 = 1; 
   }
 
   if (ev.target.className.indexOf("black") !== -1) {
-    //点击目标元素 类名中包含 black 说明是黑块
     ev.target.className = "cell";
-    ev.target.parentNode.pass = 1; //定义属性pass，表明此行row的黑块已经被点击
+    ev.target.parentNode.pass = 1;
     score();
   }
 }
-
-// 判断游戏是否结束
 
 function over() {
   var rows = con.childNodes;
@@ -74,7 +61,6 @@ function over() {
   }
 }
 
-// 游戏结束
 function fail() {
   clearInterval(clock);
   flag = false;
@@ -85,16 +71,15 @@ function fail() {
   con.style.top = "-408px";
 }
 
-// 创造一个<div class="row">并且有四个子节点<div class="cell">
 function createrow() {
   var con = $("con");
-  var row = creatediv("row"); //创建div className=row
-  var arr = creatcell(); //定义div cell的类名,其中一个为cell black
+  var row = creatediv("row"); 
+  var arr = creatcell(); 
 
-  con.appendChild(row); // 添加row为con的子节点
+  con.appendChild(row); 
 
   for (var i = 0; i < 4; i++) {
-    row.appendChild(creatediv(arr[i])); //添加row的子节点 cell
+    row.appendChild(creatediv(arr[i])); 
   }
 
   if (con.firstChild == null) {
@@ -104,15 +89,13 @@ function createrow() {
   }
 }
 
-// 创建一个类名的数组，其中一个为cell black, 其余为cell
 function creatcell() {
   var temp = ["cell", "cell", "cell", "cell"];
-  var i = Math.floor(Math.random() * 4); //随机产生黑块的位置 Math.random()函数参数 0~1的随机数
+  var i = Math.floor(Math.random() * 4); 
   temp[i] = "cell black";
   return temp;
 }
 
-//让黑块动起来
 function move() {
   var con = $("con");
   var top = parseInt(window.getComputedStyle(con, null)["top"]);
@@ -123,7 +106,7 @@ function move() {
   } else {
     top += speed;
   }
-  con.style.top = top + "px"; //不断移动top值，使它动起来
+  con.style.top = top + "px"; 
   over();
   if (top == 0) {
     createrow();
@@ -132,7 +115,6 @@ function move() {
   }
 }
 
-// 加速函数
 function speedup() {
   speed += 2;
   if (speed == 20) {
@@ -140,7 +122,6 @@ function speedup() {
   }
 }
 
-//删除某行
 function delrow() {
   var con = $("con");
   if (con.childNodes.length == 6) {
@@ -148,12 +129,10 @@ function delrow() {
   }
 }
 
-// 记分
 function score() {
-  var newscore = parseInt($("score").innerHTML) + 1; //分数加一
-  $("score").innerHTML = newscore; //修改分数
+  var newscore = parseInt($("score").innerHTML) + 1; 
+  $("score").innerHTML = newscore; 
   if (newscore % 10 == 0) {
-    //当分数是10 的倍数时使用加速函数，越来越快
     speedup();
   }
 }
